895ae2a831b482de3d147b8b02f1946470c9a0eb
Aldis Berjoza <aldis@berjoza.lv>
2019-08-25 18:36:40 +0300

    Added way to test that view itself is reloaded

--- lib/app/web/views/poc/show.rb
+++ lib/app/web/views/poc/show.rb
@@ -1,3 +1,9 @@
 class App::Web::Views::Poc::Show < ::App::Web::View
   config.template = 'poc/show'
+
+  expose :code_reload_test
+
+  def code_reload_test
+    'This however will be reloaded'
+  end
 end
